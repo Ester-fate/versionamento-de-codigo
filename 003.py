f49902af145c3c7bda24ad549e3f57fcf75c8819
ester <amorimnubia5@gmail.com>
2025-10-07 09:31:52 -0300

    Create 003.py

---
+++ 003.py
@@ -0,0 +1,7 @@
+valor1 = int(input("Digite o primeiro valor:"))
+valor2 = int(input("Digite o segundo valor:"))
+if(valor1 <= valor2):
+    print("O segundo valor é maior.")
+else:
+    if(valor1 >= valor2):
+     print("O primiero valor é maior.")
