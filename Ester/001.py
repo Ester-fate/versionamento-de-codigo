3c8947c7bc5d2e1a34e80189b15508b3b4b6f57b
ester <amorimnubia5@gmail.com>
2025-10-07 10:48:43 -0300

    ester

--- Ester/001.py
+++ Ester/001.py
@@ -2,7 +2,6 @@ ano_nasc = int(input("Digite o ano do seu nascimento:"))
 ano_atual = int(input("Digite o ano em que estamos:"))
 soma = ano_nasc - ano_atual
 print ("Você tem:", soma)
-idade = 18
 if(soma < idade):
     print ("Você ainda fará 18 anos.")
 else:
