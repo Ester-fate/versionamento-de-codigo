35bcd3632c4fbaff8b24cc2ffe1466d1873b4927
silvianicoli812-collab <silvianicoli812@gmail.com>
2025-10-09 10:33:06 -0300

    execicio 3

--- Ester/003.py
+++
@@ -1,7 +0,0 @@
-valor1 = int(input("Digite o primeiro valor:"))
-valor2 = int(input("Digite o segundo valor:"))
-if(valor1 <= valor2):
-    print("O segundo valor é maior.")
-else:
-    if(valor1 >= valor2):
-     print("O primiero valor é maior.")
