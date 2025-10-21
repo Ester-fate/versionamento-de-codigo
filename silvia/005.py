8ac58ab5d4f7fc17857456a122acf9aa14290e87
silvia <silvianicoli812@gmail.com>
2025-10-21 09:16:10 -0300

    silvia

---
+++ silvia/005.py
@@ -0,0 +1,8 @@
+print("lista com o nome da familia.")
+familia = ["Felisbela p.", "silvia n.","silvio h"]
+
+familia_ordenada = sorted(familia)
+
+print ("Nomes em ordem alfabetica:")
+for nome in familia_ordenada:
+    print(nome)
