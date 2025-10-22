2581f5f2b78e80fa58cb46d22dd1ae40617b171e
silvianicoli812-collab <silvianicoli812@gmail.com>
2025-10-22 10:10:43 -0300

    Update 007.py

--- silvia/007.py
+++ silvia/007.py
@@ -4,4 +4,10 @@ print("digite os nomes de cidades que voce deseja colocar a lista.")
 print("para terminar, digite 'sair' ou precione ENTER para finalizar.")
 while true:
     cidade = input("digita o nome de uma cidade :")
-    if 
+    if cidade == 'sair' or cidade =="":
+        break 
+    lista_cidades.append(cidade)
+    print(lista_cidades)
+    print("\nlista de cidades adicionadas:")
+    for cidade in lista_cidades:
+        print(cidade)
