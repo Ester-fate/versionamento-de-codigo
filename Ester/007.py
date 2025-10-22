c80c544b7ca9428f485fc8598fac50291511cf66
Ester-fate <amorimnubia5@gmail.com>
2025-10-22 07:24:38 -0300

    007 ester

--- Ester/007.py
+++ Ester/007.py
@@ -1,13 +1,13 @@
 print("Lista com nomes das cidades!")
 lista_cidades = []
-print("Digite os nomes de cidade que você dseja adicionar a lista:")
-print("Para terminar, digite 'sair' ou pressione ENTER para finalizar:")
+print("Digite os nomes de cidades que você dseja colocar a lista.")
+print("Para terminar, digite 'sair' ou pressione ENTER para finalizar.")
 while True:
   cidade = input("Digite o nome de uma cidade:")
-  if cidade.lower() == 'sair' or cidade == "":
+  if cidade == 'sair' or cidade == "":
     break
   lista_cidades.append(cidade)
   print(lista_cidades)
   print("\nLista de cidades adicionadas:")
   for cidade in lista_cidades:
-    print(f"- {cidade}")
+    print(cidade)
