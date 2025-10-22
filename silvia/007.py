af9cdc61882052231682a19b38e27a22d7e9c4eb
silvianicoli812-collab <silvianicoli812@gmail.com>
2025-10-22 10:02:02 -0300

    Update 007.py

---
+++ silvia/007.py
@@ -0,0 +1,7 @@
+print("lista com nomes das cidades !")
+lista_cidades =[]
+print("digite os nomes de cidades que voce deseja colocar a lista.")
+print("para terminar, digite 'sair' ou precione ENTER para finalizar.")
+while true:
+    cidade = input("digita o nome de uma cidade :")
+    if 
