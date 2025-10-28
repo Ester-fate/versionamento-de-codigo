6c31ef9915e44cc3004cefe85debc9b745bd3947
Ester-fate <amorimnubia5@gmail.com>
2025-10-28 09:13:55 -0300

    Create 003.py

---
+++ Ester/003.py
@@ -0,0 +1,11 @@
+primeiro = (int(input("Digite o primeiro valor: ")))
+segundo = (int(input("Digite o segundo valor: ")))
+
+if primeiro < segundo:
+    print("O menor valor é:", primeiro)
+
+else:
+    print ("O menor valor é:", segundo)
+
+    if primeiro == segundo: 
+          print("O maior numero é:", segundo)
