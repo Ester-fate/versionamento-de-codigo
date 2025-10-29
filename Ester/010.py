3e4a5e55a6dc4635aff8b56363eb514ecfffa79d
Ester-fate <amorimnubia5@gmail.com>
2025-10-29 10:35:04 -0300

    Update 010.py

--- Ester/010.py
+++ Ester/010.py
@@ -1,6 +1,6 @@
-lista_numeros = [10, 20, 55, 7, 48, 18, 14, 22, 26, 25, 100, 88]
-numero = input("Digite um número:")
-if numero in lista_numeros:
- print(f"O número {numero} está na lista!")
+minha_lista = [15, 8, 23, 42, 4, 16, 7, 20, 14]
+numero = int(input("Digite um número: "))
+if numero in minha_lista:
+    print(f"O número {numero} está na lista!")
 else:
- print(f"O número {numero} não está na lista!")
+    print(f"O número {numero} não foi encontrado na lista.")
