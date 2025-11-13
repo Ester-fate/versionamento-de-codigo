92a34a532406003bb1c1d870b0293326574188b1
yasminpereira <ypereira200823@gmail.com>
2025-11-13 10:05:11 -0300

    insert Iasmin

---
+++ yasmin/005.py
@@ -0,0 +1,8 @@
+print("lista com o nome da familia.")
+familia = ["lauanny.,"."gabreila", "vanderlei.","yasmin"]
+
+familia_ordenada = sorted(familia)
+
+print ("Nomes em ordem alfabetica:")
+for nome in familia_ordenada:
+    print(nome)
